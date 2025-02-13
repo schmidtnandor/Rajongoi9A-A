ed7d4f3dcc396a4fee4683d988d0a06243b9a6de
schmidtnandor <schmidt.nandor@students.jedlik.eu>
2025-02-13 11:02:23 +0100

    troll 99 százalékban kész

--- fajlok/js/troll.js
+++ fajlok/js/troll.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const trollButton = document.getElementById("troll");
     const popupImage = document.getElementById("popup-image");
     const overlay = document.getElementById("overlay");
+    const sound = new Audio(document.getElementById("popup-sound").src);
 
     if (trollButton) {
         trollButton.addEventListener("click", function () {
             popupImage.style.display = "block";
             overlay.style.display = "block";
+            sound.play();
             
             setTimeout(function () {
                 popupImage.style.display = "none";
